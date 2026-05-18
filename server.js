33c2c0515f8d8d7085551c3fcd4782eef1422d63
Nikhil p <nikhilhntr@gmail.com>
2026-05-18 11:21:49 +0000

    Update 2 files
    - /server.js
    - /backend-deployment.yaml

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // 🔐 Internal ELB (PRIVATE - only accessible inside VPC)
-const BACKEND_URL = 'http://internal-a3c53ec3847c14313982790c3fb5d9b2-1575810271.us-east-2.elb.amazonaws.com';
+const BACKEND_URL = 'http://localhost:8080';
 
 // Middleware
 app.use(express.json());
